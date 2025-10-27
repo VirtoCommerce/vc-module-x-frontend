ba14f6be969aef5a798722686b28570ddf4d22dc
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-10-27 15:42:17 +0200

    fix: sonar

--- src/VirtoCommerce.XFrontend.Web/Module.cs
+++ src/VirtoCommerce.XFrontend.Web/Module.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Xapi.Core.Extensions;
-using VirtoCommerce.Xapi.Core.Infrastructure;
 using VirtoCommerce.XFrontend.Core;
 using VirtoCommerce.XFrontend.Data;
 
@@ -17,22 +16,16 @@ public class Module : IModule, IHasConfiguration
 
     public void Initialize(IServiceCollection serviceCollection)
     {
-        // Register services
-        //serviceCollection.AddTransient<IMyService, MyService>();
-
         // Register GraphQL schema
-        var graphQlBuilder = new GraphQLBuilder(serviceCollection, builder =>
+        _ = new GraphQLBuilder(serviceCollection, builder =>
         {
             builder.AddSchema(serviceCollection, typeof(CoreAssemblyMarker), typeof(DataAssemblyMarker));
         });
-
-        serviceCollection.AddSingleton<ScopedSchemaFactory<DataAssemblyMarker>>();
     }
 
     public void PostInitialize(IApplicationBuilder appBuilder)
     {
-        // Register partial GraphQL schema
-        appBuilder.UseScopedSchema<DataAssemblyMarker>("frontend");
+
     }
 
     public void Uninstall()
