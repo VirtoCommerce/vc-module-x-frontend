6b8d349f4a243a5fecd901abcbfb594060455703
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-10-27 14:42:31 +0200

    fix: sonar

--- src/VirtoCommerce.XFrontend.Core/Queries/PageContextQuery.cs
+++ src/VirtoCommerce.XFrontend.Core/Queries/PageContextQuery.cs
@@ -15,21 +15,19 @@ public class PageContextQuery : Query<PageContexResponse>
 
     public string Permalink { get; set; }
 
-    // optional?
-    public string OrganizationId { get; set; } // whitelabeling, slugInfo
+    public string OrganizationId { get; set; }
 
-    public string UserId { get; set; } // whitelabeling, get me, slugInfo
+    public string UserId { get; set; }
     public string UserName { get; set; }
     public bool IsAnonymous { get; set; }
 
-    public string StoreId { get; set; } // store, whitelabeling, slugInfo
+    public string StoreId { get; set; }
 
     public override IEnumerable<QueryArgument> GetArguments()
     {
         yield return Argument<StringGraphType>(nameof(Domain));
         yield return Argument<StringGraphType>(nameof(CultureName));
         yield return Argument<StringGraphType>(nameof(Permalink));
-
         yield return Argument<StringGraphType>(nameof(OrganizationId));
         yield return Argument<StringGraphType>(nameof(UserId));
         yield return Argument<StringGraphType>(nameof(StoreId));
@@ -40,11 +38,8 @@ public class PageContextQuery : Query<PageContexResponse>
         Domain = context.GetArgument<string>(nameof(Domain));
         CultureName = context.GetArgument<string>(nameof(CultureName));
         Permalink = context.GetArgument<string>(nameof(Permalink));
-
         OrganizationId = context.GetArgument<string>(nameof(OrganizationId)) ?? context.GetCurrentOrganizationId();
-        //UserId = context.GetArgument<string>(nameof(UserId)) ?? context.GetCurrentUserId();
         UserId = context.GetArgument<string>(nameof(UserId));
-
         StoreId = context.GetArgument<string>(nameof(StoreId));
     }
 }
