44987ab88d45bd8b3e2d662592cb67b6daadcf97
Artem Dudarev <artem@virtoworks.com>
2025-12-02 10:30:36 +0200

    VCST-4203: Update dependencies

--- src/VirtoCommerce.XFrontend.Data/Queries/PageContextQueryBuilder.cs
+++ src/VirtoCommerce.XFrontend.Data/Queries/PageContextQueryBuilder.cs
@@ -12,7 +12,7 @@ using VirtoCommerce.XFrontend.Core.Schemas;
 
 namespace VirtoCommerce.XFrontend.Data.Queries;
 
-class PageContextQueryBuilder : QueryBuilder<PageContextQuery, PageContextResponse, PageContextResponseType>
+public class PageContextQueryBuilder : QueryBuilder<PageContextQuery, PageContextResponse, PageContextResponseType>
 {
     protected override string Name => "pageContext";
 
