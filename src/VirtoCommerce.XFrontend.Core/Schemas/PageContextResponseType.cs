d49b260823a53e66bf82bd8e93ab43e60e658caf
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-12-18 13:25:30 +0200

    refactor

--- src/VirtoCommerce.XFrontend.Core/Schemas/PageContextResponseType.cs
+++ src/VirtoCommerce.XFrontend.Core/Schemas/PageContextResponseType.cs
@@ -1,8 +1,4 @@
-using GraphQL.Resolvers;
-using GraphQL.Types;
-using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.WhiteLabeling.ExperienceApi.Schemas;
-using VirtoCommerce.Xapi.Core.Helpers;
 using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.XFrontend.Core.Models;
 using UserType = VirtoCommerce.ProfileExperienceApiModule.Data.Schemas.UserType;
@@ -16,13 +12,6 @@ public class PageContextResponseType : ExtendableGraphType<PageContextResponse>
         Field<SlugInfoResponseType>("slugInfo").Resolve(context => context.Source.SlugInfoResponse);
         Field<StoreResponseType>("store").Resolve(context => context.Source.StoreResponse);
         Field<WhiteLabelingSettingsType>("whiteLabelingSettings").Resolve(context => context.Source.WhiteLabelingSetting);
-        AddField(new FieldType
-        {
-            Name = "User",
-            Description = "User info",
-            Type = GraphTypeExtensionHelper.GetActualType<UserType>(),
-
-            Resolver = new FuncFieldResolver<PageContextResponse, ApplicationUser>(context => context.Source.User),
-        });
+        ExtendableField<UserType>(name: "user", description: "User info", resolve: context => context.Source.User);
     }
 }
