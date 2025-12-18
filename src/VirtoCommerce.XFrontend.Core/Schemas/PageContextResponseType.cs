fa20378a397c9240485852943482561809b2e0a1
Evgeny Okhrimenko <evgokhrimenko@gmail.com>
2025-12-18 16:39:49 +0200

    feat: Added support of GetActualType for User in PageContextResponseType (#2)

--- src/VirtoCommerce.XFrontend.Core/Schemas/PageContextResponseType.cs
+++ src/VirtoCommerce.XFrontend.Core/Schemas/PageContextResponseType.cs
@@ -12,6 +12,6 @@ public class PageContextResponseType : ExtendableGraphType<PageContextResponse>
         Field<SlugInfoResponseType>("slugInfo").Resolve(context => context.Source.SlugInfoResponse);
         Field<StoreResponseType>("store").Resolve(context => context.Source.StoreResponse);
         Field<WhiteLabelingSettingsType>("whiteLabelingSettings").Resolve(context => context.Source.WhiteLabelingSetting);
-        Field<UserType>("user").Resolve(context => context.Source.User);
+        ExtendableField<UserType>(name: "user", description: "User info", resolve: context => context.Source.User);
     }
 }
